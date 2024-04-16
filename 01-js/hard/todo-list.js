26f892a1a5f9f598ce4555f5a3ef694fb0f50923
Priyansh-Khati <159039441+Priyansh-Khati@users.noreply.github.com>
2024-04-16 20:00:36 +0530

    Update todo-list.js

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,64 @@
 */
 
 class Todo {
+   constructor(){
+     this.todo = [];
+   }
 
+    add(todo){
+     this.todo.push(todo);
+   }
+
+  remove(index){
+    if(index>=0 && index<todo.length){
+      this.todo.splice(index,1);
+    }
+    else{
+      console.log("Index out of range");
+    }
+      
 }
+
+  update(index, updatedVal){
+    if(index>=0 && index< todo.length){
+        this.todo[index] = updatedVal;
+    }
+    else{
+      console.log("Index out of range");
+    }
+  }
+
+  getAll(){
+    console.log(todo);
+  }
+
+  get(){
+    if(index>=0 && index < todo.length){
+      console.log(this.todo(index)];
+    }
+    else{
+      console.log("Index out of range");
+    }
+  }
+
+  clear(){
+    this.todo = [];
+  }
+  
+let myTodo = new Todo();
+myTodo.add("Completing assignment");
+myTodo.add("Going to college");
+myTodo.add("Completing 20+ videos target");
+myTodo.add("Completing 0 to 1 as soon as possible");
+myTodo.add("contributing on our group project");
+
+myTodo.getAll();
+
+myTodo.remove(2);
+myTodo.getAll();
+myTodo.update(2,"Coding atleast 3 hrs a day");
+myTodo.getAll();
+myTodo.get(2);
+    
 
 module.exports = Todo;
